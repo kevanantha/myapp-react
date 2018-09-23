154a49330809bb6ec2c70aae12d3579a2801f621
kananthav <kevin.anantha@gmail.com>
2018-09-23 14:06:15 +0800

    delete data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja.js';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yodhi', age: 24, belt: 'green', id: 2 },
+      { name: 'Mario', age: 20, belt: 'brown', id: 3 }
+    ]
+  }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+    console.log(ninjas);
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+    console.log(`Ninjas that has id: ${id} has been deleted`)
+  }
   render() {
-    return (
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My first React app!</h1>
+        <p>Welocome (:)</p>
+        <Ninjas deleteNinja={ this.deleteNinja } ninjas={ this.state.ninjas }/>
+        <AddNinja addNinja={ this.addNinja } />
       </div>
     );
   }
---
+++ src/Ninjas.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Ninjas = ({ninjas, deleteNinja}) => {
+  const ninjaList = ninjas.map(ninja => {
+    if (ninja.age > 5) {
+      return(
+        <div className="ninja" key={ninja.id}>
+          <div>Name: { ninja.name }</div>
+          <div>Age: { ninja.age }</div>
+          <div>Belt: { ninja.belt }</div>
+          <button onClick={ () => { deleteNinja(ninja.id) } }>Delete ninja</button>
+        </div>
+      )
+    } else {
+      return null;
+    }
+  });
+  return(
+    <div className="ninjas-list">
+      { ninjaList }
+    </div>
+  );
+}
+
+export default Ninjas;
